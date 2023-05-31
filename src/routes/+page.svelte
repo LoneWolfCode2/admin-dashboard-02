46872724d58ab8b9da2db29294e7f45d75df91da
Lone Wolf Code DBA Joseph S. Kertz <91030723+LoneWolfCode2@users.noreply.github.com>
2023-05-30 22:15:08 -0500

    update UI!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,35 +5,117 @@
 	let animate = false;
 	import { fade } from 'svelte/transition';
 	onMount(() => (animate = true));
+	import Icon from '@iconify/svelte';
+	let sidebarOpen = true;
+	let toggleSidebar = () => {
+		sidebarOpen = !sidebarOpen;
+	};
+	let darkMode = true;
 </script>
 
-<div class="w-screen h-screen bg-[#242529] text-white flex">
+<div class=" min-h-screen bg-[#242529] text-white flex">
 	<div
-		class="SIDEBAR w-[20%] h-screen border-r border-solid border-[rgba(255,255,255,0.1)] relative"
+		class={`SIDEBAR ${
+			sidebarOpen ? 'w-[20%]' : 'w-[72px]'
+		} p-3 h-screen border-r border-solid border-[rgba(255,255,255,0.1)] transition-all relative`}
 	>
-		<div class="LOGO w-full flex justify-center items-center py-8 sm:px-8 md:px-16 lg:px-32 gap-2">
-			<img class="flex-grow" src={logo} alt="Project logo" />
-			<div class="text-3xl">Wise</div>
+		<div
+			class="flex justify-end text-3xl"
+			on:keyup={(e) => {
+				if (e.key === 'Enter') {
+					toggleSidebar();
+				}
+			}}
+			on:click={toggleSidebar}
+		>
+			<Icon icon="material-symbols:menu" class="cursor-pointer" />
 		</div>
-		<div class="PROFILE text-center px-32">
-			<div class="rounded-full aspect-square overflow-hidden">
-				<img
-					class=""
-					src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
-					alt=""
-				/>
+
+		<div class={`${sidebarOpen ? 'opacity-100' : 'opacity-0'}`}>
+			<div class="LOGO flex justify-center items-center py-8 sm:px-8 md:px-16 lg:px-32 gap-2">
+				<img class="flex-grow" src={logo} alt="Project logo" />
+				<div class="text-3xl">Wise</div>
 			</div>
-			<div class="my-2 font-semibold">Lewis Carter</div>
+			<div class="PROFILE text-center px-32 mb-8">
+				<div class="rounded-full aspect-square overflow-hidden">
+					<img
+						class=""
+						src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+						alt=""
+					/>
+				</div>
+				<div class="my-2 font-semibold">Lewis Carter</div>
+				<div
+					class="w-fit px-4 rounded-full mx-auto mb-4 border border-solid border-[rgba(255,255,255,0.2)] hover:bg-[rgba(255,255,255,0.25)] cursor-pointer"
+				>
+					Edit
+				</div>
+			</div>
+		</div>
+		<div class="LINKS">
 			<div
-				class="w-fit px-4 rounded-full mx-auto mb-4 border border-solid border-[rgba(255,255,255,0.2)]"
+				class={'flex justify-start items-center gap-3 p-2 transition-all cursor-pointer rounded hover:bg-[rgba(255,255,255,0.25)]'}
 			>
-				Edit
+				<div class={`aspect-square  p-2`}>
+					<Icon icon="ic:round-dashboard" />
+				</div>
+				{#if sidebarOpen}
+					<div>Dashboard</div>
+				{/if}
+			</div>
+			<div
+				class={'flex justify-start items-center gap-3 p-2 transition-all cursor-pointer rounded hover:bg-[rgba(255,255,255,0.25)]'}
+			>
+				<div class={`aspect-square p-2`}>
+					<Icon icon="fluent-emoji-high-contrast:page-facing-up" />
+				</div>
+				{#if sidebarOpen}
+					<div>Activity</div>
+				{/if}
+			</div>
+			<div
+				class={'flex justify-start items-center gap-3 p-2 transition-all cursor-pointer rounded hover:bg-[rgba(255,255,255,0.25)]'}
+			>
+				<div class={`aspect-square  p-2`}>
+					<Icon icon="ph:calendar-fill" />
+				</div>
+				{#if sidebarOpen}
+					<div>Schedule</div>
+				{/if}
+			</div>
+			<div
+				class={'flex justify-start items-center gap-3 p-2 transition-all cursor-pointer rounded hover:bg-[rgba(255,255,255,0.25)]'}
+			>
+				<div class={`aspect-square p-2`}>
+					<Icon icon="basil:settings-adjust-solid" />
+				</div>
+				{#if sidebarOpen}
+					<div>Settings</div>
+				{/if}
 			</div>
 		</div>
-		<div class="LINKS" />
-		<div class="absolute bottom-4 right-0 left-0">SWITCH</div>
+		<div class="absolute bottom-4 right-0 left-0">
+			<div class={`${sidebarOpen ? 'opacity-100' : 'opacity-0'}`}>
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
+				<div class="switchContainer relative" on:click={() => (darkMode = !darkMode)}>
+					<div
+						class="h-12 w-[150px] mx-auto rounded-full bg-[rgba(255,255,255,0.2)] border border-solid border-grey flex items-center"
+					>
+						<div class="flex mx-3 justify-between w-full">
+							<div><Icon icon="bi:sun-fill" /></div>
+							<div><Icon icon="teenyicons:moon-solid" /></div>
+						</div>
+						<div
+							class={`absolute ml-1 h-10 aspect-square bg-[#FFF47A] rounded-full transition-all ${
+								darkMode ? 'translate-x-[100px]' : null
+							}`}
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
-	<div class="MAIN w-[80%] p-8">
+	<div class={`MAIN ${sidebarOpen ? 'w-[80%]' : 'calcWidth'} p-8`}>
 		<div class="grid grid-cols-3 w-full md:gap-12 lg:gap-24">
 			{#if animate}
 				{#each gridItems as item, i}
@@ -70,5 +152,8 @@
 	}
 	.bg-\[\#36383E\] {
 		background-color: #36383e;
+	}
+	.calcWidth {
+		width: calc(100vw - 64px);
 	}
 </style>
